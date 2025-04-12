9d58030c6d2ef72fcf7fc5cfccca95482a99db66
Duong Ba Khang <justJackiee>
2025-04-13 00:36:39 +0700

    killall

--- src/sys_killall.c
+++ src/sys_killall.c
@@ -12,6 +12,8 @@
 #include "syscall.h"
 #include "stdio.h"
 #include "libmem.h"
+#include "queue.h"
+#include <string.h>
 
 int __sys_killall(struct pcb_t *caller, struct sc_regs* regs)
 {
@@ -33,16 +35,48 @@ int __sys_killall(struct pcb_t *caller, struct sc_regs* regs)
     }
     printf("The procname retrieved from memregionid %d is \"%s\"\n", memrg, proc_name);
 
-    /* TODO: Traverse proclist to terminate the proc
-     *       stcmp to check the process match proc_name
-     */
-    //caller->running_list
-    //caller->mlq_ready_queu
+    /* Traverse running_list to terminate matching processes */
+    struct pcb_t *proc;
+    struct queue_t *running_list = caller->running_list;
+    
+    if (running_list != NULL) {
+        for (int i = 0; i < running_list->size; i++) {
+            proc = running_list->proc[i];
+            if (strcmp(proc->path, proc_name) == 0) {
+                printf("Terminating process %d with name %s from running list\n", 
+                       proc->pid, proc_name);
+                // Remove from running list
+                for (int j = i; j < running_list->size - 1; j++) {
+                    running_list->proc[j] = running_list->proc[j + 1];
+                }
+                running_list->size--;
+                i--; // Adjust index since we removed an element
+            }
+        }
+    }
 
-    /* TODO Maching and terminating 
-     *       all processes with given
-     *        name in var proc_name
-     */
+#ifdef MLQ_SCHED
+    /* Traverse mlq_ready_queue to terminate matching processes */
+    struct queue_t *mlq_ready_queue = caller->mlq_ready_queue;
+    if (mlq_ready_queue != NULL) {
+        for (int i = 0; i < MAX_PRIO; i++) {
+            struct queue_t *queue = &mlq_ready_queue[i];
+            for (int j = 0; j < queue->size; j++) {
+                proc = queue->proc[j];
+                if (strcmp(proc->path, proc_name) == 0) {
+                    printf("Terminating process %d with name %s from ready queue\n", 
+                           proc->pid, proc_name);
+                    // Remove from ready queue
+                    for (int k = j; k < queue->size - 1; k++) {
+                        queue->proc[k] = queue->proc[k + 1];
+                    }
+                    queue->size--;
+                    j--; // Adjust index since we removed an element
+                }
+            }
+        }
+    }
+#endif
 
     return 0; 
 }
